67be76b6455b231b5a827b8d619d6b2952b34789
Oleg Melnikov <olegm72@gmail.com>
2022-09-09 01:30:13 +0200

    CharDisappearing changed a little

--- CharDisappearing/CharDisappearing/Program.cs
+++ CharDisappearing/CharDisappearing/Program.cs
@@ -9,10 +9,11 @@ namespace CharDisappearing
             do
             {
                 Console.Clear();
-                Console.WriteLine("This is a text test. Press Esc to test and Enter to exit");
-                Console.WriteLine("This");
-                ConsoleKey c = Console.ReadKey().Key;
-                if (c == ConsoleKey.Enter) break;
+                Console.WriteLine("This is the first line. If you press Esc when running, the first char disappears. Press Enter to exit");
+                Console.WriteLine("This is a second line. The first letter does not disappear in it.");
+                
+                ConsoleKey c = Console.ReadKey().Key; // using ReadKey(true) solves the problem but does not show the reason of the problem
+                if (c == ConsoleKey.Enter) break;   // leave the endless cycle
             }
             while (true);
         }
