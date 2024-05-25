2eee2224c3c9e8a9e4360b0ba412e84e91f62c7f
Oleg Melnikov <olegm72@gmail.com>
2024-05-25 23:40:01 +0200

    Added a program

---
+++ PasswordEncrypt/PasswordEncrypt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PasswordEncrypt
+{
+    internal class Program
+    {
+        private static string EncryptOrDecryptPassword(string password)
+        // in this case, encryption and decryption use the same operation :)
+        {
+            string result = "";
+            for (int i = 0; i < password.Length; i++)
+                result += (char)(password[i] ^ Int16.MaxValue);   // XOR with 1111111111111111
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Enter password");
+            string s = Console.ReadLine();
+            Console.WriteLine("Encrypted password: ");
+            string es = EncryptOrDecryptPassword(s);
+            Console.WriteLine(es + "\r\nDecrypted password: ");
+            string des = EncryptOrDecryptPassword(es);
+            Console.WriteLine(des);
+        }
+    }
+}
